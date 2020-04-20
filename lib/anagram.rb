29b544e3215d067845f5196ab7e1432cb1edba4e
Dina Kendle <dina.kendle@gmail.com>
2020-04-20 20:21:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
   def match(array)
     new_array = []
     array.each do |element| 
-      if @word.split("") == (words.split("")
+      if @word.split("") == words.split("")
         new_array << element
       end
     end
