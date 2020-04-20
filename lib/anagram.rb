e25739bf725c1b969ad5eacad443fcf8dcbd43a5
Dina Kendle <dina.kendle@gmail.com>
2020-04-20 19:38:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  @@words = []
+  
+  def initialize
+    @word = word
+    @@words << self
+  end
+  
+  
+  def self.match
+    
+    @@words.detect {|words| }
+  end
+  
+end
